26d74a1d89b3f4e1ce2af436d8f23ec31d68ba52
TasigurIshtar <47100974+TasigurIshtar@users.noreply.github.com>
2020-02-28 09:06:47 +0100

    First atempt

---
+++ XbimWebUITest/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XbimWebUITest.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+   
+    }
+}
